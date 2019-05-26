2673cb0efe68b14fb886977b71e5e4d7f0623cc8
Dylan <dylanfairbanks@icloud.com>
2019-05-25 20:40:54 -0600

    uploading unity project including scripts for dots and player controls

---
+++ GregariousGame/Assets/Scripts/DotClass.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DotClass : MonoBehaviour {
+
+  [Range(0.0f, 1.0f)]
+  public  float hue;
+  [Range(0.3f, 1.0f)]
+  public  float saturation;
+  [Range(0.0f, 1.0f)]
+  public  float brightness;
+  private SpriteRenderer sprite;
+
+  //constructor
+  public DotClass(float _hue, float _saturation, float _brightness)
+  {
+    SetColor(_hue, _saturation, _brightness);
+  }
+
+
+	// Use this for initialization
+	void Start () 
+  {
+    sprite = gameObject.GetComponent<SpriteRenderer>();
+    SetColor(229, 30, 96);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    sprite.color = Color.HSVToRGB(hue, saturation, brightness);
+  }
+
+  public void SetColor(float _hue, float _saturation, float _brightness)
+  {
+    hue = _hue / 360;
+    saturation = _saturation / 100;
+    brightness = _brightness / 100;
+
+  }
+}
---
+++ GregariousGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(DotClass))]
+public class PlayerController : MonoBehaviour 
+{
+
+  private Rigidbody2D rigidbody2D;
+  private float xDirection = 0.0f;
+  private float yDirection = 0.0f;
+  [SerializeField]
+  private float speed = 1.0f;
+
+
+  // Use this for initialization
+  void Start () 
+  {
+    rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    gameObject.GetComponent<DotClass>().SetColor(1, 72, 88);
+    
+  }
+  
+  // Update is called once per frame
+  void Update () 
+  {
+    xDirection = Input.GetAxisRaw("Horizontal");
+    yDirection = Input.GetAxisRaw("Vertical");
+  }
+
+  private void FixedUpdate()
+  {
+    var velocityX = xDirection * speed;
+    var velocityY = yDirection * speed;
+    rigidbody2D.velocity = new Vector2(velocityX, velocityY);
+  }
+}
